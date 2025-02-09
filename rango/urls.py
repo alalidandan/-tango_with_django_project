06eabf2ab90060a3125a267a7e1d5e6dac9f87e3
alalidana <2960892A@student.gla.ac.uk>
2025-02-09 15:07:51 +0000

    added a photo and served media (chapter 4.2/4.3)

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rango import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     #maps `/rango/` to the `index` view
     path('', views.index, name='index'),
     #maps '/rango/about/' to 'about'
     path('about/', views.about, name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
