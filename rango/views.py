9039fed9e3df031c189406fc4b639bfc246edaf7
alalidana <2960892A@student.gla.ac.uk>
2025-02-14 11:25:41 +0000

    Implemented visit tracking using cookies and updated index view to display visit count.

--- rango/views.py
+++ rango/views.py
@@ -7,8 +7,10 @@ from rango.forms import UserForm, UserProfileForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
 
 # Create your views here.
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -19,7 +21,9 @@ def index(request):
         'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"
     }
 
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 
 def about(request):
     context_dict = {'boldmessage': 'Dana'}
@@ -139,3 +143,17 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse("rango:index"))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time) > timedelta(days=1):
+        visits += 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
